ceb40d39113fd07bda1e08f163f7062d7a8ab741
admin <admin@DESKTOP-GODRBNL>
2024-08-29 11:55:46 +0530

    Add project files.

---
+++ RealEstateApi/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApi.Data;
+using RealEstateApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RealEstateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ApiDbContext _dbContext = new ApiDbContext();
+
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _dbContext.Categories;
+        }
+
+        // GET api/<CategoriesController>/5
+        [HttpGet("{id}")]
+        public Category Get(int id)
+        {
+            return _dbContext.Categories.FirstOrDefault(c => c.Id==id);
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public void Post([FromBody] Category category)
+        {
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Category category)
+        {
+            var obj = _dbContext.Categories.Find(id);
+            obj.Name = category.Name;
+            obj.ImageUrl = category.ImageUrl;
+            _dbContext.SaveChanges();
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var category = _dbContext.Categories.Find(id);
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ RealEstateApi/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstateApi.Models;
+
+namespace RealEstateApi.Data
+{
+    public class ApiDbContext: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\ProjectModels;database=RealEstateDB");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
