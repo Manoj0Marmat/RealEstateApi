368ea5b523d256104206766801e2793b3a88cc15
Leo <manojrajput762000@gmail.com>
2024-09-01 13:11:30 +0530

    feat: login with jwt token

--- RealEstateApi/Controllers/UsersController.cs
+++ RealEstateApi/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RealEstateApi.Data;
 using RealEstateApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace RealEstateApi.Controllers
 {
@@ -10,6 +15,12 @@ namespace RealEstateApi.Controllers
     public class UsersController : ControllerBase
     {
         ApiDbContext _dbContext = new ApiDbContext();
+        private IConfiguration _config;
+
+        public UsersController(IConfiguration config)
+        {
+            _config = config;
+        }
 
         [HttpPost("[Action]")]
         public IActionResult Register([FromBody] User user)
@@ -26,5 +37,35 @@ namespace RealEstateApi.Controllers
 
             return StatusCode(StatusCodes.Status201Created);
         }
+
+        [HttpPost("[Action]")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var currentUser = _dbContext.Users.FirstOrDefault(u => u.Email==user.Email && u.Password==user.Password);
+
+            if (currentUser is null) {
+                return NotFound();
+            }
+
+            // generate JWT Token 
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: credentials
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(jwt);
+        }
     }
+
 }
